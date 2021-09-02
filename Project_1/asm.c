4cf27419f4ca5d058815195f9ed50eb6dcfe703a
mtowerss <mtowers74@gmail.com>
2021-09-02 17:38:48 -0400

    New approach to asm.c

--- Project_1/asm.c
+++ Project_1/asm.c
@@ -3,25 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
-struct Map {
-  struct Key key;
-  struct Value value;
-};  
+void printRegisters();
+void mov(char *reg, char *constant);
+char registerReturn(char *opCode);
 
-struct Key {
-    struct point p1;
-    struct point p2;
-    int i;
-    int j;
-};
+char *R0, *R1, *R2, *R3, *R4, *R5, *R6, *R7, *R8, *R9, *R10, *R11, *R12, *R13, *R14, *R15, *R16, *RPC;
 
-int get_hash(Key *key){
-    int result;
-    result = key->i * key->i + (key->p1.x * key->p1.x) - (key->p2.x * key->p2.x)
-    return (result % SIZE_OF_MAP);
-}
-*/
 int main(void) { 
   FILE *file;
   char filename[200];
@@ -29,7 +16,6 @@ int main(void) {
   size_t len = 0;
   ssize_t read;
   char *opCode;
- // struct Map map[SIZE_OF_MAP];
 
  // gets(filename);
   file = fopen("testcase.text","r");
@@ -39,31 +25,90 @@ int main(void) {
   while ((read = getline(&newLine, &len, file)) != -1) { 
     printf("%s", newLine);	  
     opCode = strtok(newLine, " ");
+    if(strcmp("beq", opCode) != 0 && strcmp("b", opCode) !=0) {
+      char *reg = strtok(NULL, " ");	    
+      if(strcmp("R1", reg) == 0)
+        	      
+    }
+
 
     if(strcmp("mov", opCode) == 0){
-      char *reg = strtok(NULL, " ");	    
       char *constant = strtok(NULL, " ");
-     /*  
-      printf("%s ", opCode);
-      printf("%s ", reg);
-      printf("%s", constant); */
-    }		    
+      if(reg
+      mov(reg, constant);
+    }
+    else if (strcmp("ldr", opCode) == 0) {
+
+    }
+    else if (strcmp("ldi", opCode) == 0) {
+
+    }
+    else if (strcmp("str", opCode) == 0) {
+
+    }	    
+    else if (strcmp("sti", opCode) == 0) {
+
+    }
+    else if (strcmp("add", opCode) == 0) {
+
+    }
+    else if (strcmp("mul", opCode) == 0) {
+
+    }
+    else if (strcmp("cmp", opCode) == 0) {
+
+    }
+    else if (strcmp("b", opCode) == 0) {
+
+    }
+    else if (strcmp("beq", opCode) == 0) {
+
+    }
   }
-  for(int i = 0; i <= 9; i++){
-    printf("register 0x%d: %p\n",i,&i);
-  }	  
-  for(char i = 'A'; i <= 'F'; i++){
-    printf("register 0x%c: %p\n",i,&i); 
-  }	  
+  printRegisters();
+
   fclose(file);
+
   if(newLine)
     free(newLine);
   exit(EXIT_SUCCESS);
   return 0; 
    
 }
+char registerReturn(char *registerVal) {
 
-//void mov(char *reg, char *constant){
-  
+}	
+
+void mov(char *reg, char *constant) {
+  reg = constant;
+	
+}
 
 
+
+
+
+
+
+
+
+void printRegisters(){
+  printf("register 0x0: %s\n",R0);
+  printf("register 0x1: %s\n",R1);
+  printf("register 0x2: %s\n",R2);
+  printf("register 0x3: %s\n",R3); 
+  printf("register 0x4: %s\n",R4);
+  printf("register 0x5: %s\n",R5);
+  printf("register 0x6: %s\n",R6);
+  printf("register 0x7: %s\n",R7);
+  printf("register 0x8: %s\n",R8);
+  printf("register 0x9: %s\n",R9);
+  printf("register 0xA: %s\n",R11);
+  printf("register 0xB: %s\n",R12);
+  printf("register 0xC: %s\n",R13);
+  printf("register 0xD: %s\n",R14);
+  printf("register 0xE: %s\n",R15);
+  printf("register 0xF: %s\n",R16);
+  printf("register  PC: %s\n",RPC);
+
+}	
