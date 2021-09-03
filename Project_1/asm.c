edc6013559eba3c2ed296155f5a1365dd4609d52
mtowerss <mtowers74@gmail.com>
2021-09-03 17:23:10 -0400

    made changex to asm.c

--- Project_1/asm.c
+++ Project_1/asm.c
@@ -4,10 +4,32 @@
 #include <string.h>
 
 void printRegisters();
+void printMemory();
 void mov(char *reg, char *constant);
+void ldr(char *reg, int memLocation);
+void ldi(char *reg1, char *reg2);
+void str(char *reg, int memLocation);
 char registerReturn(char *opCode);
-
+char *defaultVal = "0x000000000000";
 char *R0, *R1, *R2, *R3, *R4, *R5, *R6, *R7, *R8, *R9, *R10, *R11, *R12, *R13, *R14, *R15, *R16, *RPC;
+char values[17][15];
+char memory[65][15];
+  
+void fillMemory(){
+	for(int i = 0; i < 64; i++){
+	  for(int j = 0; j < 12; j++){
+	    memory[i][j] = defaultVal[j];
+	  }
+	}
+}	
+
+void fillValues(){
+	for(int i = 0; i < 17; i++){
+	  for(int j = 0; j < 12; j++){
+	    values[i][j] = defaultVal[j];
+	  }
+	} 
+}
 
 int main(void) { 
   FILE *file;
@@ -17,6 +39,8 @@ int main(void) {
   ssize_t read;
   char *opCode;
 
+  fillMemory();
+  fillValues();
  // gets(filename);
   file = fopen("testcase.text","r");
   if (file == NULL) {
@@ -25,26 +49,25 @@ int main(void) {
   while ((read = getline(&newLine, &len, file)) != -1) { 
     printf("%s", newLine);	  
     opCode = strtok(newLine, " ");
-    if(strcmp("beq", opCode) != 0 && strcmp("b", opCode) !=0) {
-      char *reg = strtok(NULL, " ");	    
-      if(strcmp("R1", reg) == 0)
-        	      
-    }
-
+    char *reg = strtok(NULL, " ");	    
 
     if(strcmp("mov", opCode) == 0){
       char *constant = strtok(NULL, " ");
-      if(reg
       mov(reg, constant);
     }
     else if (strcmp("ldr", opCode) == 0) {
-
+      char *memLocation = strtok(NULL, " ");
+      int location = atoi(memLocation);
+      ldr(reg, location);
     }
     else if (strcmp("ldi", opCode) == 0) {
-
+      char *reg2 = strtok(NULL, " ");
+      ldi(reg, reg2);      
     }
     else if (strcmp("str", opCode) == 0) {
-
+      char *memLocation = strtok(NULL, " ");
+      int location = atoi(memLocation);
+      str(reg, location);
     }	    
     else if (strcmp("sti", opCode) == 0) {
 
@@ -66,8 +89,10 @@ int main(void) {
     }
   }
   printRegisters();
-
+  printMemory();
   fclose(file);
+  
+      
 
   if(newLine)
     free(newLine);
@@ -75,13 +100,33 @@ int main(void) {
   return 0; 
    
 }
-char registerReturn(char *registerVal) {
-
-}	
 
 void mov(char *reg, char *constant) {
-  reg = constant;
+  for(int i = 0; i < 17; i++){
+    values[((int)reg[1])-48][i] = constant[i];
+  }	    
+ // values[(int)reg[1]] = constant;
 	
+}
+
+void ldr(char *reg, int memLocation) {
+  for(int i = 0; i < 15; i++){	  
+    values[((int)reg[1])-48][i] = memory[memLocation][i];
+  }    
+}
+
+void ldi(char *reg, char *reg2) {
+  int reg3 = atoi(reg);
+  int reg4 = atoi(reg2);  
+  for(int i = 0; i < 15; i++) {
+    values[reg3][i] = values[reg4][i];	  
+  }
+}
+
+void str(char *reg, int memLocation) {
+  for(int i = 0; i < 15; i++){	  
+    memory[memLocation][i] = values[((int)reg[1])-48][i] ;
+  }    
 }
 
 
@@ -92,23 +137,59 @@ void mov(char *reg, char *constant) {
 
 
 
-void printRegisters(){
-  printf("register 0x0: %s\n",R0);
-  printf("register 0x1: %s\n",R1);
-  printf("register 0x2: %s\n",R2);
-  printf("register 0x3: %s\n",R3); 
-  printf("register 0x4: %s\n",R4);
-  printf("register 0x5: %s\n",R5);
-  printf("register 0x6: %s\n",R6);
-  printf("register 0x7: %s\n",R7);
-  printf("register 0x8: %s\n",R8);
-  printf("register 0x9: %s\n",R9);
-  printf("register 0xA: %s\n",R11);
-  printf("register 0xB: %s\n",R12);
-  printf("register 0xC: %s\n",R13);
-  printf("register 0xD: %s\n",R14);
-  printf("register 0xE: %s\n",R15);
-  printf("register 0xF: %s\n",R16);
-  printf("register  PC: %s\n",RPC);
+void printMemory(){
+  printf("%s","\n");	  
+  for(int i = 0; i < 65; i++){
+    if( i == 0) {
+      printf("%s:  ", "0x00");      
+    }	    
+    if( i == 8) {
+      printf("\n%s:  ", "0x08");      
+    }
+    if( i == 16) {
+      printf("\n%s:  ", "0x10");      
+    }
+    if( i == 24) {
+      printf("\n%s:  ", "0x18");      
+    }
+    if( i == 32) {
+      printf("\n%s:  ", "0x20");      
+    }
+    if( i == 40) {
+      printf("\n%s:  ", "0x28");      
+    }
+    if( i == 48) {
+      printf("\n%s:  ", "0x30");      
+    }
+    if( i == 56) {
+      printf("\n%s:  ", "0x38");      
+    }
+    if((i%8 == 0) && i != 0){
+      printf("%s ", memory[i]);	    
+    } else {  
+      printf("%s ", memory[i]);
+    }
+  }  
 
+}
+
+
+void printRegisters(){
+  printf("register 0x0: %s\n",values[0]);
+  printf("register 0x1: %s",values[1]);
+  printf("register 0x2: %s",values[2]);
+  printf("register 0x3: %s\n",values[3]); 
+  printf("register 0x4: %s\n",values[4]);
+  printf("register 0x5: %s\n",values[5]);
+  printf("register 0x6: %s\n",values[6]);
+  printf("register 0x7: %s\n",values[7]);
+  printf("register 0x8: %s\n",values[8]);
+  printf("register 0x9: %s\n",values[9]);
+  printf("register 0xA: %s\n",values[10]);
+  printf("register 0xB: %s\n",values[11]);
+  printf("register 0xC: %s\n",values[12]);
+  printf("register 0xD: %s\n",values[13]);
+  printf("register 0xE: %s\n",values[14]);
+  printf("register 0xF: %s\n",values[15]);
+  printf("register  PC: %s\n",values[16]);
 }	
